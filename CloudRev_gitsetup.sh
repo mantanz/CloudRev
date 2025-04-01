68b9fc6ddfe3d00ec7a70e8105475f232dc645b4
manish-taneja <manish.taneja01@gmail.com>
2025-04-01 13:26:12 +0530

    updated git setup

--- CloudRev_gitsetup.sh
+++ CloudRev_gitsetup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 git remote set-url origin git@github.com:mantanz/CloudRev.git
-git config user.name "manish-taneja"
-git config user.email "manish.taneja01@gmail.com"
 git checkout dev-branch
 echo "Switched repository and configured settings!"
